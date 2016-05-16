bfb49be5fd815c9e1a69dc75c66e4b9eb49e14ac
Eystein <thatnorwegian@gmail.com>
2016-05-16 14:30:39 +0200

    Add configs

--- fractal.js
+++ fractal.js
@@ -16,14 +16,24 @@ fractal.set('docs.path', 'docs');
 /*
  * Tell the Fractal web preview plugin where to look for static assets.
  */
-fractal.set('plugins.web.static.path', 'public');
+fractal.set('plugins.web.static.path', 'assets');
 
 /*
  * Give your project a title.
  */
-fractal.set('project.title', 'My Component Library');
+fractal.set('project.title', 'UpMySport Component Library');
 
 /*
  * The default layout template to use when rendering component previews
  */
 fractal.set('components.default.preview', '@preview');
+
+/*
+ * The default status to use for a component. Choices: prototype | wip | ready. Default: ready
+ */
+fractal.set('components.default.status', 'ready');
+
+/*
+ * The directory within which any static builds of the web UI should be generated.
+ */
+fractal.set('plugins.web.build.path', 'build');
